2de13d690297c075a24d7253efdc638072f0c98d
uzulla <uzulla@himitsukichi.com>
2018-05-08 12:43:16 +0900

    update some improvement

--- exec.php
+++ exec.php
@@ -1,8 +1,8 @@
 <?php
 ini_set('error_reporting', -1);
 ini_set('display_errors', 'On');
-ini_set('log_errors', 'On');
-ini_set('error_log', __DIR__.'/error.log');
+//ini_set('log_errors', 'On');
+//ini_set('error_log', __DIR__.'/error.log');
 // strict error bailout
 function strict_error_handler($errno, $errstr, $errfile, $errline)
 {
@@ -48,11 +48,22 @@ foreach($items as $item){
     $content = $item->getContent();
     $name = $item->getName();
 
+    error_log($name);
+
+    // check funny package name
+    $num = preg_match_all("/\-/u", $name);
+    if($num>5){
+        error_log ("SKIP too many - {$name}" );
+        $lastdate = $item->getDate();
+        continue;
+    }
+
     $packagist_json_api_url = "https://packagist.org/packages/{$name}.json";
     $api_json = file_get_contents($packagist_json_api_url);
     $api_data = json_decode($api_json,1);
     if(is_null($api_data)){
         // failed. skip!
+        error_log( "SKIP packagist json api access failed. {$name}");
         $lastdate = $item->getDate();
         continue;
     }
@@ -60,18 +71,55 @@ foreach($items as $item){
     $repo_url = $api_data['package']['repository'];
 
     $context = stream_context_create(array(
-        'http' => ['ignore_errors' => true],
+        'http' => [
+            'ignore_errors' => true,
+            'header' => 'User-Agent: PHP',
+        ],
     ));
     file_get_contents($repo_url, false, $context);
     
+    // check repo exists
     if (
         strpos($http_response_header[0], '200') === false &&
         strpos($http_response_header[0], '302') === false  // for gitlab.
     ) {
-        // not exists repo, maybe SPAM?
-        error_log("not 200 SKIP: " . $repo_url . " - " . $http_response_header[0]);
+        error_log("SKIP repo url not 200 " . $repo_url . " - " . $http_response_header[0]);
         $lastdate = $item->getDate();
         continue;
+    }
+
+    // check user info
+    if( strpos($repo_url, 'github') !== false ){
+        $match = preg_match("|https://github.com/([^/]+)/|u", $repo_url, $m);
+        $gh_user_name = $m[1];
+
+        $gh_user_data = json_decode(file_get_contents("https://api.github.com/users/{$gh_user_name}", false, $context),1);
+        if(is_null($gh_user_data)){
+            error_log("SKIP gh user data is null {$name}");
+            $lastdate = $item->getDate();
+            continue;
+        }
+
+        if($gh_user_data['type']=="User"){
+
+            $point = 0;
+            // if($gh_user_data['public_repos']<3) $point ++;
+            if($gh_user_data['public_gists']==0) $point ++;
+            if($gh_user_data['following']==0) $point ++;
+            if($gh_user_data['followers']==0) $point ++;
+            if($gh_user_data['name']==NULL) $point ++;
+            if($gh_user_data['company']==NULL) $point ++;
+            if($gh_user_data['blog']==NULL) $point ++;
+            // if($gh_user_data['location']==NULL) $point ++;
+            // if($gh_user_data['email']==NULL) $point ++;
+            // if($gh_user_data['bio']==NULL) $point ++;
+
+            if($point > 4){
+                error_log("SKIP like robot {$name}");
+                $lastdate = $item->getDate();
+                continue;
+            }
+        }
     }
     
     $content = preg_replace('|https?://[a-zA-Z0-9/:%#&~=_!\'\$\?\(\)\.\+\*]+|u', '<snip url>', $content);
