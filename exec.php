4e5ba6da5c840c683bd67ce152d7d042483295e1
uzulla <uzulla@himitsukichi.com>
2020-07-11 19:11:26 +0900

    Tidy up many things.

--- exec.php
+++ exec.php
@@ -1,10 +1,15 @@
 <?php
 // strict error bailout
+use FastFeed\Factory;
+use FastFeed\Item;
+use Uzulla\Util\Twitter;
+
 function strict_error_handler($errno, $errstr, $errfile, $errline)
 {
     error_log("STRICT: {$errno} {$errstr} {$errfile} {$errline} ");
-    die("STRICT: {$errno} {$errstr} {$errfile} {$errline} ".PHP_EOL);
+    die("STRICT: {$errno} {$errstr} {$errfile} {$errline} " . PHP_EOL);
 }
+
 set_error_handler("strict_error_handler");
 
 require "vendor/autoload.php";
@@ -12,30 +17,30 @@ require "config.php";
 
 //twitter setup
 $user_values = [
-    'twitter_oauth_token'=>TWITTER_ACCESS_TOKEN,
-    'twitter_oauth_token_secret'=>TWITTER_ACCESS_TOKEN_SECRET
+    'twitter_oauth_token' => TWITTER_ACCESS_TOKEN,
+    'twitter_oauth_token_secret' => TWITTER_ACCESS_TOKEN_SECRET
 ];
-\Uzulla\Util\Twitter::setConsumerKey(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+Twitter::setConsumerKey(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
 
 //drift file
 if (!file_exists(LAST_DATE_FILE)) {
-    $lastdate = new DateTime('2000-01-01 00:00:00');
+    $last_date = new DateTime('2000-01-01 00:00:00');
 } else {
-    $lastdate = new DateTime(file_get_contents(LAST_DATE_FILE));
+    $last_date = new DateTime(file_get_contents(LAST_DATE_FILE));
 }
 
 //read rss
 $newly_submitted_packages_rss = "https://packagist.org/feeds/packages.rss";
 
-$ff = \FastFeed\Factory::create();
+$ff = Factory::create();
 $ff->addFeed('new_submit', $newly_submitted_packages_rss);
 
-/** @var \FastFeed\Item[] $items */
+/** @var Item[] $items */
 $items = $ff->fetch('new_submit');
 $items = array_reverse($items); // RSSは新しいのが上にくる（っぽい）ので。
 
 foreach ($items as $item) {
-    if ($lastdate->format('U') >= $item->getDate()->format('U')) {
+    if ($last_date->format('U') >= $item->getDate()->format('U')) {
         // already processed, skip!
         continue;
     }
@@ -44,9 +49,9 @@ foreach ($items as $item) {
     $name = $item->getName();
 
     // check funny package name
-    if (preg_match_all("/\-/u", $name)>5) {
+    if (preg_match_all("/-/u", $name) > 5) {
         error_log("SKIP too many '-' {$name}");
-        $lastdate = $item->getDate();
+        $last_date = $item->getDate();
         continue;
     }
 
@@ -56,13 +61,13 @@ foreach ($items as $item) {
     $api_data = json_decode($api_json, 1);
     if (is_null($api_data)) { // failed. skip!
         error_log("SKIP packagist json api access failed. {$name}");
-        $lastdate = $item->getDate();
+        $last_date = $item->getDate();
         continue;
     }
 
     // try to get repo url
     $repo_url = $api_data['package']['repository'];
-    if(!preg_match('|^https://|u', $repo_url)){
+    if (!preg_match('|^https://|u', $repo_url)) {
         error_log("SKIP repo url is not https {$repo_url}"); // I found "git@gitlab.com〜" pattern. 
         continue;
     }
@@ -73,14 +78,14 @@ foreach ($items as $item) {
         ],
     ));
     file_get_contents($repo_url, false, $context);
-    
+
     // check repo exists
     if (
         strpos($http_response_header[0], '200') === false &&
         strpos($http_response_header[0], '302') === false  // for gitlab.
     ) {
         error_log("SKIP repo url not 200|302 " . $repo_url . " - " . $http_response_header[0]);
-        $lastdate = $item->getDate();
+        $last_date = $item->getDate();
         continue;
     }
 
@@ -98,7 +103,7 @@ foreach ($items as $item) {
         $gh_user_data = json_decode(file_get_contents("https://api.github.com/users/{$gh_user_name}", false, $context), 1);
         if (is_null($gh_user_data)) {
             error_log("SKIP gh user data is null {$name} {$repo_url}");
-            $lastdate = $item->getDate();
+            $last_date = $item->getDate();
             continue;
         }
 
@@ -106,29 +111,29 @@ foreach ($items as $item) {
         $yesterday_at = new DateTime('-2 days');
         if ($gh_created_at > $yesterday_at) {
             error_log("SKIP gh account too new {$name} {$repo_url}");
-            $lastdate = $item->getDate();
+            $last_date = $item->getDate();
             continue;
         }
     }
-    
+
     // snip url. that is spammers link in many cases. I don't want get DMCA mail.
-    $content = preg_replace('|https?://[a-zA-Z0-9/:%#&~=_!\'\$\?\(\)\.\+\*]+|u', '<snip url>', $content);
+    $content = preg_replace('|https?://[a-zA-Z0-9/:%#&~=_!\'$?().+*]+|u', '<snip url>', $content);
     $str = "[New]{$item->getName()} {$content}";
-    if (mb_strlen($str)>TWEET_MAX_LENGTH_WITHOUT_URL) {
-        $str = mb_substr($str, 0, TWEET_MAX_LENGTH_WITHOUT_URL)."…";
+    if (mb_strlen($str) > TWEET_MAX_LENGTH_WITHOUT_URL) {
+        $str = mb_substr($str, 0, TWEET_MAX_LENGTH_WITHOUT_URL) . "…";
     }
     $str = "{$str} {$repo_url}";
     try {
         if (TWITTER_CONSUMER_KEY!="") {
             \Uzulla\Util\Twitter::sendTweet($user_values, $str);
         }
-    } catch (\Exception $e) {
+    } catch (Exception $e) {
         error_log($e->getMessage());
-        echo "Twitter post fail: ".$e->getMessage().PHP_EOL;
+        echo "Twitter post fail: " . $e->getMessage() . PHP_EOL;
     }
-    echo $str."\n";
+    echo $str . "\n";
     sleep(1); // post wait.
-    $lastdate = $item->getDate();
+    $last_date = $item->getDate();
 }
 
-file_put_contents(LAST_DATE_FILE, $lastdate->format('c')); // update last
+file_put_contents(LAST_DATE_FILE, $last_date->format('c')); // update last
--- lib/Uzulla/Util/Twitter.php
+++ lib/Uzulla/Util/Twitter.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace Uzulla\Util;
+
+use Codebird\Codebird;
+use Exception;
 
 class Twitter
 {
@@ -15,73 +19,90 @@ class Twitter
         return self::$_instance;
     }
 
-    public static function getCb()
+    public static function getCb(): Codebird
     {
-        return \Codebird\Codebird::getInstance();
+        return Codebird::getInstance();
     }
 
     public static function setConsumerKey($key, $secret)
     {
-        \Codebird\Codebird::setConsumerKey($key, $secret);
+        Codebird::setConsumerKey($key, $secret);
     }
 
     public function setToken($token, $secret)
     {
-        $this->_oauth_token        = $token;
+        $this->_oauth_token = $token;
         $this->_oauth_token_secret = $secret;
     }
 
+    /**
+     * @param $user_value
+     * @return mixed
+     * @throws Exception
+     */
     static public function getMe($user_value)
     {
         $cb = self::getCb();
         $cb->setToken($user_value['twitter_oauth_token'], $user_value['twitter_oauth_token_secret']);
 
-        try{
+        try {
             $res = $cb->account_verifyCredentials();
-            if($res->httpstatus != 200){
-                throw new \Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
+            if ($res->httpstatus != 200) {
+                throw new Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
             }
-            if(!$res){
-                throw new \Exception('null response');
+            if (!$res) {
+                throw new Exception('null response');
             }
-            if(!property_exists($res, 'id_str')){
-                throw new \Exception('not found id_str');
+            if (!property_exists($res, 'id_str')) {
+                throw new Exception('not found id_str');
             }
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
 
         return $res;
     }
 
+    /**
+     * @param $user_value
+     * @param $screen_name
+     * @return mixed
+     * @throws Exception
+     */
     static public function getByScreenName($user_value, $screen_name)
     {
         $cb = self::getCb();
         $cb->setToken($user_value['twitter_oauth_token'], $user_value['twitter_oauth_token_secret']);
 
-        try{
-            $res = $cb->users_show(['screen_name'=>$screen_name]);
-            if($res->httpstatus != 200){
-                throw new \Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
+        try {
+            $res = $cb->users_show(['screen_name' => $screen_name]);
+            if ($res->httpstatus != 200) {
+                throw new Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
             }
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
 
         return $res;
     }
 
+    /**
+     * @param $user_value
+     * @param $twitter_id
+     * @return mixed
+     * @throws Exception
+     */
     static public function getByTwitterId($user_value, $twitter_id)
     {
         $cb = self::getCb();
         $cb->setToken($user_value['twitter_oauth_token'], $user_value['twitter_oauth_token_secret']);
 
-        try{
-            $res = $cb->users_show(['user_id'=>$twitter_id]);
-            if($res->httpstatus != 200){
-                throw new \Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
+        try {
+            $res = $cb->users_show(['user_id' => $twitter_id]);
+            if ($res->httpstatus != 200) {
+                throw new Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
             }
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
 
@@ -89,108 +110,147 @@ class Twitter
     }
 
 
-    static public function getTweetByScreenName($user_value, $screen_name, $include_rt=false, $limit=10, $since_id=null)
+    /**
+     * @param $user_value
+     * @param $screen_name
+     * @param bool $include_rt
+     * @param int $limit
+     * @param null $since_id
+     * @return array
+     * @throws Exception
+     */
+    static public function getTweetByScreenName($user_value, $screen_name, $include_rt = false, $limit = 10, $since_id = null)
     {
         $cb = self::getCb();
         $cb->setToken($user_value['twitter_oauth_token'], $user_value['twitter_oauth_token_secret']);
 
-        try{
+        try {
             $api = 'statuses/userTimeline';
             $params['screen_name'] = $screen_name; // App owner - the app has read access
             $params['include_rts'] = $include_rt;
             $params['count'] = $limit;
-            if($since_id != null){
+            if ($since_id != null) {
                 $params['since_id'] = $since_id;
             }
-            $res = (array) $cb->$api($params);
-            if($res['httpstatus'] != 200){
-                error_log(print_r($cb,1));
-                error_log(print_r($res,1));
-                throw new \Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
+            $res = $cb->$api($params);
+            if ($res['httpstatus'] != 200) {
+                error_log(print_r($cb, 1));
+                error_log(print_r($res, 1));
+                throw new Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
             }
             unset($res['httpstatus']);
 
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
         return $res;
     }
 
+    /**
+     * @param $user_value
+     * @param $screen_name
+     * @return mixed
+     * @throws Exception
+     */
     static public function followByScreenName($user_value, $screen_name)
     {
         $cb = self::getCb();
         $cb->setToken($user_value['twitter_oauth_token'], $user_value['twitter_oauth_token_secret']);
 
-        try{
-            $res = $cb->friendships_create(['screen_name'=>$screen_name]);
-            if($res->httpstatus != 200){
-                throw new \Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
+        try {
+            $res = $cb->friendships_create(['screen_name' => $screen_name]);
+            if ($res->httpstatus != 200) {
+                throw new Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
             }
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
 
         return $res;
     }
 
+    /**
+     * @param $user_value
+     * @param $screen_name
+     * @return bool
+     * @throws Exception
+     */
     static public function followCheckByScreenName($user_value, $screen_name)
     {
         $cb = self::getCb();
         $cb->setToken($user_value['twitter_oauth_token'], $user_value['twitter_oauth_token_secret']);
 
-        try{
-            $res = $cb->friendships_lookup(['screen_name'=>$screen_name]);
-            if($res->httpstatus != 200){
-                throw new \Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
+        try {
+            $res = $cb->friendships_lookup(['screen_name' => $screen_name]);
+            if ($res->httpstatus != 200) {
+                throw new Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
             }
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
 
         $res_array = (array)$res;// $res->0 に相当するのってどうやって指定するの…。
         $connections_list = $res_array['0']->connections;
 
-        foreach($connections_list as $connections){
-            if($connections=='none'){
+        foreach ($connections_list as $connections) {
+            if ($connections == 'none') {
                 return false;
-            }else if($connections=='following'){
+            } else if ($connections == 'following') {
                 return true;
-            }else if($connections=='followed_by'){
+            } else if ($connections == 'followed_by') {
                 continue;
             }
         }
-        throw new \Exception('Twitter API response invalid.');
+        throw new Exception('Twitter API response invalid.');
     }
 
-    static public function getOtherSizeProfileImageUrl($url, $type='normal')
+    /**
+     * @param $url
+     * @param string $type
+     * @return string|string[]|null
+     * @throws Exception
+     */
+    static public function getOtherSizeProfileImageUrl($url, $type = 'normal')
     {
-        if(!preg_match('/\A(normal|bigger|mini|original)\z/', $type)){
-            throw new \Exception('Type must (normal|bigger|mini|original)');
+        if (!preg_match('/\A(normal|bigger|mini|original)\z/', $type)) {
+            throw new Exception('Type must (normal|bigger|mini|original)');
         }
-        $new_url = preg_replace('/_(normal|bigger|mini|original)\./', "_{$type}.", $url);
-        return $new_url;
+        return preg_replace('/_(normal|bigger|mini|original)\./', "_{$type}.", $url);
     }
 
-    static public function sendTweet($user_value, $text){
+    /**
+     * @param $user_value
+     * @param $text
+     * @return mixed
+     * @throws Exception
+     */
+    static public function sendTweet($user_value, $text)
+    {
         $cb = self::getCb();
         $cb->setToken($user_value['twitter_oauth_token'], $user_value['twitter_oauth_token_secret']);
 
-        try{
+        try {
             $res = $cb->statuses_update([
                 'status' => $text
             ]);
-            if($res->httpstatus != 200){
-                throw new \Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
+            if ($res->httpstatus != 200) {
+                throw new Exception("Twitter api response {$res->httpstatus} -> {$res->errors[0]->message}");
             }
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
         return $res;
     }
 
-    static public function getAuthUrl($callback_url){
-        try{
-            $cb = \Codebird\Codebird::getInstance();
+    /**
+     * @param $callback_url
+     * @return string
+     * @throws Exception
+     */
+    static public function getAuthUrl($callback_url)
+    {
+        try {
+            $cb = Codebird::getInstance();
             $reply = $cb->oauth_requestToken(array(
                 'oauth_callback' => $callback_url
             ));
@@ -201,20 +261,25 @@ class Twitter
             $_SESSION['oauth_verify'] = true;
 
             $auth_url = $cb->oauth_authenticate();
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
         return $auth_url;
     }
 
-    static public function oauthCallbackGetTokenList(){
-        if( !isset($_GET['oauth_verifier']) || !isset($_SESSION['oauth_verify']) ){
-            throw new \Exception('twitter auth fail');
+    /**
+     * @return array
+     * @throws Exception
+     */
+    static public function oauthCallbackGetTokenList()
+    {
+        if (!isset($_GET['oauth_verifier']) || !isset($_SESSION['oauth_verify'])) {
+            throw new Exception('twitter auth fail');
         }
         //unset($_SESSION['oauth_verify']);
 
-        try{
-            $cb = \Codebird\Codebird::getInstance();
+        try {
+            $cb = Codebird::getInstance();
             $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
             $reply = $cb->oauth_accessToken(['oauth_verifier' => $_GET['oauth_verifier']]);
             var_dump($reply);
@@ -222,10 +287,9 @@ class Twitter
             return [
                 'twitter_oauth_token' => $reply->oauth_token,
                 'twitter_oauth_token_secret' => $reply->oauth_token_secret,
-                ];
-        }catch(\Exception $e){
+            ];
+        } catch (Exception $e) {
             throw $e;
         }
     }
-
 }
